07dd8cf8d6ff242042f1244b28490c6a84d8df92
smsimba225 <smsimba225@gmail.comgit commit -m Add>
2024-03-08 18:35:05 -0800

    Updates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,34 @@
 
 
-
-
 import csv
+
+candidate_list=[]
+candidatename=[]
+candidatevote=[]
+candidatepercent=[]
+totalvotes=0
+with open('./resources/election_data.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+
+    next(csv_reader)
+
+    for line in csv_reader:
+        totalvotes=totalvotes+1
+        candidate_list.append(line[2]) 
+           
+for x in set(candidate_list):
+        candidatename.append(x)
+        candidatevote.append(candidate_list.count(x))
+        candidatepercent.append(candidate_list.count(x)/totalvotes*100)
+
+print(candidatename)
+truncatedpercent=[]
+for item in candidatepercent:
+    truncated_value = round(item,3)
+    truncatedpercent.append(str(truncated_value))
+
+for i in range(len(candidatename)):
+        print(candidatename[i] + ": " + str(truncatedpercent[i]) + "% (" + str(candidatevote[i])+ ")")
+
+winner=candidatename[candidatepercent.index(max(candidatepercent))]
+print(winner)
--- Pybank/main.py
+++ Pybank/main.py
@@ -32,30 +32,27 @@ with open('./resources/budget_data.csv', 'r') as csv_file:
         starting_profit=ending_profit #sets the starting profit to equal the last ending profit
         
    
-    deltaprofitloss[0] = 0 #changes first change in profit 
+    deltaprofitloss[0] = 0 #changes first change in profit because first line doesn't count towards change in profits
     sumdelta = sum(deltaprofitloss)
-    #print(deltaprofitloss)
-    avgchangeprofit=round(sumdelta/(int(totalmonths)-1),2) # -1 is because first line doesn't count towards average
+    avgchangeprofit=round(sumdelta/(int(totalmonths)-1),2) # -1 is because first line doesn't count towards average of change in profits
     greatest_increase = max(deltaprofitloss)
     greatest_decrease = min(deltaprofitloss)
     date_greatest_increase=date[deltaprofitloss.index(greatest_increase)]
     date_greatest_decrease=date[deltaprofitloss.index(greatest_decrease)]
-    #print (totalmonths)
-    #print (round(avgchangeprofit, 2))
  
     print ('\n'+ 'Financial Analysis'+ '\n')
     print ('----------------------------'+"\n")
-    print ('Total Months:' + ' ' + str(totalmonths)+ '\n')
-    print ('Total:' + ' ' + '$' + str(totalprofitloss)+ '\n')
-    print ('Average Change:' + ' ' + '$' + str(avgchangeprofit)+ '\n')
-    print ('Greatest Increase in Profits:' + ' ' + str(date_greatest_increase) + ' ' + '($' + str(greatest_increase)+')'+ '\n')
-    print ('Greatest Decrease in Profits:' + ' ' + str(date_greatest_decrease) + ' ' + '($' + str(greatest_decrease)+')'+ '\n')
+    print ('Total Months: ' + str(totalmonths)+ '\n')
+    print ('Total: ' + '$' + str(totalprofitloss)+ '\n')
+    print ('Average Change: ' + '$' + str(avgchangeprofit)+ '\n')
+    print ('Greatest Increase in Profits: ' + str(date_greatest_increase) + ' ($' + str(greatest_increase)+')'+ '\n')
+    print ('Greatest Decrease in Profits: ' + str(date_greatest_decrease) + ' ($' + str(greatest_decrease)+')'+ '\n')
  
 with open('financial_analysis.txt', 'w') as text:
-    text.write("Financial Analysis"+ "\n")
-    text.write("--------------------------\n\n")
-    text.write("Total Months: " + str(totalmonths) + "\n")
-    text.write("Total Profits: " + "$" + str(totalprofitloss) +"\n")
-    text.write("Average Change: " + '$' + str(int(avgchangeprofit)) + "\n")
-    text.write("Greatest Increase in Profits: " + str(date_greatest_increase) + " ($" + str(greatest_increase) + ")\n")
-    text.write("Greatest Decrease in Profits: " + str(date_greatest_decrease) + " ($" + str(greatest_decrease) + ")\n")
+    text.write('Financial Analysis'+ '\n')
+    text.write('--------------------------\n\n')
+    text.write('Total Months: ' + str(totalmonths) + '\n')
+    text.write('Total Profits: ' + '$' + str(totalprofitloss) + '\n')
+    text.write('Average Change: ' + '$' + str(int(avgchangeprofit)) + '\n')
+    text.write('Greatest Increase in Profits: ' + str(date_greatest_increase) + ' ($' + str(greatest_increase) + ')\n')
+    text.write('Greatest Decrease in Profits: ' + str(date_greatest_decrease) + ' ($' + str(greatest_decrease) + ')\n')
