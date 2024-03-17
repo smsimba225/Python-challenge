000874e04c3adc227c09145d1534c9f667be861b
smsimba225 <smsimba225@gmail.comgit commit -m Add>
2024-03-17 11:45:26 -0700

    Update with comments.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,30 +8,41 @@ candidatename=[]
 candidatevote=[]
 candidatepercent=[]
 totalvotes=0
+
+# code to make path to csv file compatible across OS
 csvpath = os.path.join('Resources', 'election_data.csv')
+# also puts analysis in appropriate folder as per instructions
+txtpath = os.path.join('analysis', 'election_results.txt')
+
+# code to read csv file
+
 with open(csvpath) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
 
     next(csv_reader)
 
     for line in csv_reader:
-        totalvotes=totalvotes+1
-        candidate_list.append(line[2]) 
+        totalvotes=totalvotes+1 # to count total number of votes, adds 1 to counter as it loops
+        candidate_list.append(line[2]) # adds name of candidate to list of candidates to find all candidates in election
 
-candidate_set=sorted(set(candidate_list), key=candidate_list.index)   #sorted required to keep set order the same as when in a list, set because we don't want duplicates.
+# turn candidate list into set - 
+# sorted required to keep set order the same as when in a list, since set usually doesn't keep order but we want set because we don't want duplicates.
+candidate_set=sorted(set(candidate_list), key=candidate_list.index)   
 
 for x in candidate_set: 
         candidatename.append(x)
         candidatevote.append(candidate_list.count(x))
         candidatepercent.append(candidate_list.count(x)/totalvotes*100)
 
-truncatedpercent=[] #https://www.geeksforgeeks.org/python-ways-to-format-elements-of-given-list/ helped for truncating decimals
+truncatedpercent=[] # https://www.geeksforgeeks.org/python-ways-to-format-elements-of-given-list/ helped for truncating decimals
 for item in candidatepercent:
     truncated_value = round(item,3)
     truncatedpercent.append(str(truncated_value))
 
+# determines winner by comparing percentages
 winner=candidatename[candidatepercent.index(max(candidatepercent))]
 
+# code to print results
 print('\n'+'Election Results'+ '\n')
 print('---------------------------------'+'\n')
 print('Total Votes: ' + str(totalvotes) + '\n')
@@ -44,7 +55,9 @@ print('---------------------------------'+'\n')
 print('Winner: ' + winner + '\n')
 print('---------------------------------'+'\n')
 
-with open('election_results.txt', 'w') as text:
+# code to write election results to text file:
+
+with open(txtpath, 'w') as text:
         text.write('\n'+'Election Results'+ '\n\n')
         text.write('----------------------------'+'\n\n')
         text.write('Total Votes: ' + str(totalvotes) + '\n\n')
--- Pybank/main.py
+++ Pybank/main.py
@@ -14,25 +14,31 @@ totalmonths=0
 totalprofitloss=0
 starting_profit=0
 ending_profit=0
-csvpath = os.path.join('Resources', 'budget_data.csv')
-with open(csvpath) as csv_file:
+# makes path to csv file compatible across OS
+csvpath = os.path.join('Resources', 'budget_data.csv') 
+
+# also puts txt file in appropriate folder according to instructions
+txtpath = os.path.join('analysis', 'financial_analysis.txt') 
+
+# reads csv file
+with open(csvpath) as csv_file: 
     csv_reader = csv.reader(csv_file, delimiter=",")
     
     next(csv_reader)
 
     for line in csv_reader:
-        totalmonths = totalmonths+1 # adds 1 to counter
+        totalmonths = totalmonths+1 # adds 1 to counter to find total number of months
         date.append(line[0])
         profit.append(line[1])
         totalprofitloss = totalprofitloss + int(line[1])
         ending_profit=int(line[1])
-        changeprofit = int(ending_profit)-int(starting_profit) #calculate change in profit
+        changeprofit = int(ending_profit)-int(starting_profit) # calculate change in profit
         
-        deltaprofitloss.append(changeprofit) #add change to change profit list
-        starting_profit=ending_profit #sets the starting profit to equal the last ending profit
+        deltaprofitloss.append(changeprofit) # add change to change profit list
+        starting_profit=ending_profit # sets the starting profit to equal the last ending profit
         
    
-    deltaprofitloss[0] = 0 #changes first change in profit because first line doesn't count towards change in profits
+    deltaprofitloss[0] = 0 # changes first change in profit because first line doesn't count towards change in profits
     sumdelta = sum(deltaprofitloss)
     avgchangeprofit=round(sumdelta/(int(totalmonths)-1),2) # -1 is because first line doesn't count towards average of change in profits
     greatest_increase = max(deltaprofitloss)
@@ -40,6 +46,7 @@ with open(csvpath) as csv_file:
     date_greatest_increase=date[deltaprofitloss.index(greatest_increase)]
     date_greatest_decrease=date[deltaprofitloss.index(greatest_decrease)]
  
+# code to display financial analysis
     print ('\n'+ 'Financial Analysis'+ '\n')
     print ('----------------------------'+"\n")
     print ('Total Months: ' + str(totalmonths)+ '\n')
@@ -48,7 +55,8 @@ with open(csvpath) as csv_file:
     print ('Greatest Increase in Profits: ' + str(date_greatest_increase) + ' ($' + str(greatest_increase)+')'+ '\n')
     print ('Greatest Decrease in Profits: ' + str(date_greatest_decrease) + ' ($' + str(greatest_decrease)+')'+ '\n')
  
-with open('financial_analysis.txt', 'w') as text:
+ # code to write financial analysis to text
+with open(txtpath, 'w') as text:
     text.write('Financial Analysis'+ '\n')
     text.write('--------------------------\n\n')
     text.write('Total Months: ' + str(totalmonths) + '\n')
